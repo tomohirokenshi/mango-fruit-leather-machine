7a078ddc19c32bb9f4ceebb15f4fe8b3dfffc892
Timothy Kenneth Dantic <timothykennethdantic@gmail.com>
2025-01-07 19:44:40 +0800

    Update dispenser.ino

--- dispenser.ino
+++ dispenser.ino
@@ -50,5 +50,79 @@ void setup() {
 }
 
 void loop() {
+  // 1st Step: Set Up Temperature (Minimum = Ambient, Maximum = 70degC)
+  while (1) {
+    lcd.setCursor(0, 0); lcd.print("A:- B:+ C:DONE");
+    lcd.setCursor(0, 1); lcd.print("Temp: "); lcd.print(tempTH);
+    if (!digitalRead(btnA)) {
+      delay(200);
+      tempTH -= 5;
+      if (tempTH < 0) {
+        tempTH = 0;
+      }
+    }
+    if (!digitalRead(btnB)) {
+      delay(200);
+      tempTH += 5;
+      if (tempTH > 70) {
+        tempTH = 70;
+      }
+    }
+    if (!digitalRead(btnC)) {
+      delay(200);
+      break;
+    }
+  }
+  lcd.clear();
 
+  // 2nd Step: Set Up Duration
+  while (1) {
+    lcd.setCursor(0, 0); lcd.print("A:- B:+ C:DONE");
+    if (mins == 5) {
+      lcd.setCursor(0, 1); lcd.print("Time: 05");
+    } else {
+      lcd.setCursor(0, 1); lcd.print("Time: "); lcd.print(mins);
+    }
+    if (!digitalRead(btnA)) {
+      mins -= 5;
+      delay(200);
+      if (mins < 0) {
+        mins = 0;
+      }
+    }
+    if (!digitalRead(btnB)) {
+      mins += 5;
+      delay(200);
+    }
+    if (!digitalRead(btnC)) {
+      break;
+    }
+  }
+  delay(500);
+  lcd.clear();
+
+  // Turn ON Dehydrator Elements and Buzzer
+  digitalWrite(heater, HIGH);
+  digitalWrite(fans, HIGH);
+  preHeat();
+  digitalWrite(buzzer, HIGH);
+  delay(1000);
+  digitalWrite(buzzer, LOW);
+  lcd.clear();
+  showTimer(mins);
+  digitalWrite(buzzer, HIGH);
+  delay(1000);
+  digitalWrite(buzzer, LOW);
+  digitalWrite(heater, LOW);
+  digitalWrite(fans, LOW);
+}
+
+// Preheat Function
+void preHeat() {
+  timeNow = millis();
+  lcd.clear();
+  lcd.setCursor(0, 1); lcd.print("T: "); lcd.print(temp, 2); lcd.print("C H: "); lcd.print(hum, 0); lcd.print("%");
+  lcd.setCursor(0, 0); lcd.print("PREHEATING");
+  while (1) {
+  }
 }
