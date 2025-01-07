3211f3215a5700a26905c59838210406e13ece96
Timothy Kenneth Dantic <timothykennethdantic@gmail.com>
2025-01-07 19:25:01 +0800

    Update dispenser.ino

--- dispenser.ino
+++ dispenser.ino
@@ -10,7 +10,7 @@
 
 // LCD Display
 #include <Wire.h>
-#include "LiquidCrystal_I2C.h>
+#include "LiquidCrystal_I2C.h"
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2);
 
 // Temperature Sensor
